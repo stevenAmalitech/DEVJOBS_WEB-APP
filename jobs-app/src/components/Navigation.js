05b92a41f0f68363cfe74469087dd1a92f7c5f38
emmens4u <emmanuel.mensah@amalitech.org>
2022-03-13 07:52:28 +0800

    toggle mode button

--- jobs-app/src/components/Navigation.js
+++ jobs-app/src/components/Navigation.js
@@ -7,8 +7,12 @@ function Navigation() {
   <div className="container-fluid">
     <span className="navbar-brand text-white mb-0 h1 bbb">devjobs</span>
   </div>
-  
-
+<div className="flex-container">
+<label class="switch">
+  <input type="checkbox"/>
+  <span class="slider round"></span>
+</label>
+</div>
 
 </div>
   )
