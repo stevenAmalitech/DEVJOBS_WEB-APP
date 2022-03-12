01f1d1f8f5a7b8e7301c80852615606e35579a45
emmens4u <emmanuel.mensah@amalitech.org>
2022-03-12 08:22:49 +0800

    progress navbar

--- jobs-app/src/App.js
+++ jobs-app/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import React, { Component }  from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './components/Navigation';
+import './components/Navigation.css';
 
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+
+
+class App extends Component {
+  render () {
+    return( 
+    <div className="container-fluid position-relative">
+      <Navigation />
+
+     </div>
+     );
+   
+  
+    }
 }
+
 
 export default App;
---
+++ jobs-app/src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    
+<div className="aaa triangle">
+  <div className="container-fluid">
+    <span className="navbar-brand text-white mb-0 h1 bbb">devjobs</span>
+  </div>
+  
+
+
+</div>
+  )
+}
+
+export default Navigation;
