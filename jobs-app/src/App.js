6f4c818e14d241143e45166adf8255f18c8e0b60
Amudzi Elorm Precious <precious.amudzi@amalitech.org>
2022-03-13 23:11:36 +0000

    navbar

--- jobs-app/src/App.js
+++ jobs-app/src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import NavigationBar from './components/NavigationBar';
+import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <NavigationBar/>
+      <Home />
     </div>
   );
 }
