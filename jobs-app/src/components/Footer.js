62b632879b217375b0fe80d0ffceded34665e0bd
emmens4u <emmanuel.mensah@amalitech.org>
2022-03-14 13:25:16 +0800

    Footer component done

---
+++ jobs-app/src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+<footer className="text-center text-white fixed-bottom" >
+    
+        
+  
+  
+  <div className="text-center p-3" >
+    <h4>Senior Software Engineer</h4>
+    <p className="text-white">
+        <h6>So Digital Inc</h6> </p>
+        <button type="button" class="btn btn-primary btn-sm apply">Apply Now</button>
+  </div>
+  
+  
+
+  
+  
+</footer>
+    
+  )
+}
+
+export default Footer;
